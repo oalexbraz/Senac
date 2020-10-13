aec528053e14949fc9b661db8de20d3010d8a42e
oalexbraz <alex.braz@anexconsultoria.com.br>
2020-10-13 19:10:37 -0300

    Criação do Inscreva-se

--- app/src/main/java/com/example/projeto_padrao/MainActivity.java
+++ app/src/main/java/com/example/projeto_padrao/MainActivity.java
@@ -4,17 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.LogPrinter;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.minha_tela);
+        setContentView(R.layout.meu_layout);
 
         Log.d( "MEU_LOG",  "LOG 1");
         Log.d( "MEU_LOG",  "LOG 2");
         Log.d( "MEU_LOG",  "LOG 3");
     }
+
+    @Override
+    protected void onStart (){
+        Log.d("ciclo_de_vida" , "onResume - Estado de interação com a tela");
+            super.onStart();
+    }
+    @Override
+    protected void onPause(){
+        Log.d("cliclo_de_vida" , "onPause - inicio o término da activity");
+            super.onPause();
+    }
+    @Override
+    protected void onStop(){
+        Log.d("cliclo_de_vida" , "onStop - A atividade não está mais visivel ao usuario");
+
+    }
+    @Override
+    protected void onDestroy(){
+        Log.d("cliclo_de_vida", "onDestroy - A Activity foi complemente destruida");
+            super.onDestroy();
+    }
+
 }
-]
